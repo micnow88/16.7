00c5cf7c9c4a72373d72ab520c865d8724482b27
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-07-10 11:16:40 +0200

    Add files in js

--- js/App.js
+++ js/App.js
@@ -1,12 +1,44 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '4200',
+  'X-Auth-Token': '5e945a744ede38cef9f26db211e36a92'
+};
+
+//funkcja pobierająca tablice z serwera
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+ });
+
+//funkcja tworząca kolumny z danych z serwera
+function setupColumns(columns) {
+  columns.forEach(function(column) {
+		var col = new Column(column.id, column.name);
+      board.addColumn(col);
+      setupCards(col, column.cards);
+  });
+}
+
+//funkcja tworząca karty z danych z serwera
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
+
 // OGÓLNA FUNKCJA
-function randomString() {
+/*function randomString() {
 	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
 	var str = '', i;
 	for (i = 0; i < 10; i++) {
 	  str += chars[Math.floor(Math.random() * chars.length)];
 	}
 	return str;
-}
+}*/
 
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
@@ -18,7 +50,7 @@ function generateTemplate(name, data, basicElement) {
   	return element;
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
+/*// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
 var todoColumn = new Column('Do zrobienia');
 var doingColumn = new Column('W trakcie');
 var doneColumn = new Column('Skończone');
@@ -34,4 +66,4 @@ var card2 = new Card('stworzyc tablice kanban');
 
 // DODAWANIE KART DO KOLUMN
 todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+doingColumn.createCard(card2);*/
--- js/Board.js
+++ js/Board.js
@@ -9,8 +9,22 @@ var board = {
 
 document.querySelector('#board .create-column').addEventListener('click', function() {
     var name = prompt('Enter a column name');
-    var column = new Column(name);
-    board.addColumn(column);
+    var data = new FormData();
+
+    data.append('name', name);
+
+    fetch(baseUrl + '/column', {
+      method: 'POST',
+      headers: myHeaders,
+      body: data,
+    })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(resp) {
+      var column = new Column(resp.id, name);
+      board.addColumn(column);
+    });
 });
 	
 function initSortable(id) {
